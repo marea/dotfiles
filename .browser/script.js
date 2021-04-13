dc752c01834e630df3c2709c20b653db19164d0f
marea <m.arevalo.aragon@gmail.com>
2021-04-13 03:31:20 -0500

    Changed structure and styling

--- .browser/script.js
+++ .browser/script.js
@@ -1,6 +1,6 @@
 const quote = quotes[Math.floor(Math.random()*quotes.length)]
-const quoteElement = document.querySelector('#quote')
-const authorElement = document.querySelector('#author')
+const quoteElement = document.querySelector('.quote__text')
+const authorElement = document.querySelector('.quote__author')
 
 quoteElement.innerText = quote.quoteText
 authorElement.innerText = quote.quoteAuthor
