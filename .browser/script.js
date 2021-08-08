d7edd4632b9cfe5583ba0db829017cd0d168d066
marea <m.arevalo.aragon@gmail.com>
2021-08-08 13:10:00 -0500

    Removed startpage

--- .browser/script.js
+++
@@ -1,6 +0,0 @@
-const quote = quotes[Math.floor(Math.random()*quotes.length)]
-const quoteElement = document.querySelector('.quote__text')
-const authorElement = document.querySelector('.quote__author')
-
-quoteElement.innerText = quote.quoteText
-authorElement.innerText = quote.quoteAuthor
