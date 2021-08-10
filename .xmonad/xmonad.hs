b49f89cd693b29309533e8910c78d3ee5d81f396
marea <m.arevalo.aragon@gmail.com>
2021-08-10 00:38:57 -0500

    Added emojipick shortcut

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -43,6 +43,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
     , ((modm,               xK_r     ), spawn myDmenuCommand)
     , ((modm,               xK_p     ), spawn "bitwarden-dmenu --dmenu-args='-b' --dmenu-pswd-args='-b'")
+    , ((modm,               xK_grave ), spawn "/home/marea/.bin/emojipick")
     , ((modm,               xK_e     ), spawn "alacritty -e nnn")
 
     , ((modm .|. shiftMask, xK_q     ), kill)
