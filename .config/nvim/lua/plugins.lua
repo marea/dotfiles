b42754d04b0f8dbfe334caab4b40ac707be602c0
marea <m.arevalo.aragon@gmail.com>
2021-08-14 00:43:10 -0500

    Added indent-blankline.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,15 +5,15 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+	fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+	execute 'packadd packer.nvim'
 end
 
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'duggiefresh/vim-easydir'
+	use 'wbthomason/packer.nvim'
+	use 'duggiefresh/vim-easydir'
 	use 'kyazdani42/nvim-web-devicons'
 	use {
 		'kyazdani42/nvim-tree.lua',
@@ -58,13 +58,26 @@ require('packer').startup(function()
 	--	end
 	--}
 	use {
-    'NTBBloodbath/rest.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
+		'NTBBloodbath/rest.nvim',
+		requires = { 'nvim-lua/plenary.nvim' }
 	}
 	use {
 		'windwp/nvim-ts-autotag',
 		config = function()
 			require('nvim-ts-autotag').setup()
+		end
+	}
+	use {
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			vim.g.indent_blankline_char = "┊"
+			vim.g.indent_blankline_filetype_exclude = {
+				"log","gitcommit", "packer", "markdown", "json", "txt", "help",
+				"NvimTree", "git", ""
+			}
+			vim.g.indent_blankline_buftype_exclude = {"terminal", "nofile"}
+			-- because lazy load indent-blankline so need readd this autocmd
+			vim.cmd('autocmd CursorMoved * IndentBlanklineRefresh')
 		end
 	}
 end)
