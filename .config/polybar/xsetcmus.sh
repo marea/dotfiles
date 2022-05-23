08d57f82168b72d9a3ed6e86396b004bab1c71dd
marea <m.arevalo.aragon@gmail.com>
2022-05-23 08:37:15 -0500

    Added xsetcmus script and configuration

---
+++ .config/polybar/xsetcmus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if ps -C cmus > /dev/null; then
+
+artist=`cmus-remote -Q | 
+	grep --text '^tag artist' | 
+	sed '/^tag artistsort/d' | 
+	awk '{gsub("tag artist ", "");print}'`
+title=`cmus-remote -Q  | 
+	grep --text '^tag title' | 
+	sed -e 's/tag title //' |
+	awk '{gsub("tag title ", "");print}'`
+
+	echo " $title - $artist"; else echo ""; 
+fi
